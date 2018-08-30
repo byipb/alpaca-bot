1732f2811a26145ac192f0d419c1d3b421145f12
orangepeel <byipgit@outlook.com>
2018-08-30 02:51:51 -0700

    Added self-assigning Cloud role.

--- src/main/java/Alpaca.java
+++ src/main/java/Alpaca.java
@@ -1,10 +1,17 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.managers.GuildController;
 
 import javax.security.auth.login.LoginException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Alpaca extends ListenerAdapter {
 
@@ -24,8 +31,33 @@ public class Alpaca extends ListenerAdapter {
         }
     }
 
+    private void deleteMessage(MessageReceivedEvent event) throws InterruptedException {
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+            event.getMessage().delete();
+            event.getMessage().delete();
+        }
+
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
-    public void onMessageReceived(MessageReceivedEvent event){
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        // Setup
+        Guild guild = event.getGuild();
+        GuildController guildController = new GuildController(guild);
+        TextChannel textChannel = event.getTextChannel();
+        Member member = event.getMember();
+        List<Role> roles = guild.getRoles();
+
+        // List of roles
+        //Role snowflake = roles.get(3);
+        Role cloud = roles.get(4);
+
         if (event.getAuthor().isBot()){
             return;
         }
@@ -38,8 +70,18 @@ public class Alpaca extends ListenerAdapter {
         if (event.getMessage().getContentRaw().equals("!ping")){
             //remember to call queue()!
             //otherwise our message will never be sent
-            event.getChannel().sendMessage("Pong!").queue();
+            event.getChannel().sendMessage("Pamf!").queue();
         }
+
+        // Give a user the Cloud role
+        if (event.getMessage().getContentRaw().equals(".iam Cloud")){
+
+            // Alpaca bot gives Role
+            guildController.addSingleRoleToMember(member, cloud).queue();
+            event.getChannel().sendMessage("You're a cloud now, "+ event.getMember().getUser().getName() + "!").queue();
+        }
+
+        // TODO: clean up ice-prison message spam by deleting member message and bot message
 
     }
 }
